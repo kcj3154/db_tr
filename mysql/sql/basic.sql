080c4a9c5371d14650325e060ca397b5f1891cdb
kcj3154 <kcj0531@naver.com>
2022-07-28 14:15:52 +0900

    20220728_2

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,3 +1,17 @@
 -- CJ2 database를 사용
-use CJ2;
+USE CJ2;
+
+-- 전체 컬럼 조회
+SELECT * from member2;
+
+-- 컬럼 추가
+-- ALTER TABLE member2 ADD COLUMN nickname varchar(45);
+
+-- ALTER TABLE member2 ADD COLUMN nameEng varchar(45) AFTER name
+
+-- 컬럼 변경
+-- ALTER TABLE member2 MODIFY COLUMN nickname varchar(100);
+
+-- 컬럼 이름 변경
+-- ALTER TABLE member2 CHANGE COLUMN nickname nick varchar(45);
 
