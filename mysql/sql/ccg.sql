0c26b490d99d59c8ee74c22123f2c3db5c0d22e2
kcj3154 <kcj0531@naver.com>
2022-09-02 17:31:41 +0900

    20220902

--- mysql/sql/ccg.sql
+++ mysql/sql/ccg.sql
@@ -1,1 +1,17 @@
+use CJ2;
+
 SELECT * FROM CJ2.ccg;
+
+SELECT
+			a.seq
+			,a.codeGroup
+			,a.codeGroupEn
+			,a.useNy
+			,a.reg_date
+			,a.mod_date
+			,COUNT(*) cnt
+		FROM ccg a
+		LEFT JOIN cc b on a.seq = b.ccg_seq
+		GROUP BY a.seq
+        
+        
